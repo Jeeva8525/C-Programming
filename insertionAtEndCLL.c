915a85881526e81740a43bac09631cc8e5bc2797
Jeeva8525 <jeevananthan8507@gmail.com>
2025-08-02 17:32:10 +0530

    performs the insertion operation at the end of the circular linked list

---
+++ insertionAtEndCLL.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct circularLinkedList
+{
+    int data;
+    struct circularLinkedList *next;
+} CLL;
+void insEnd(CLL **start)
+{
+    int val;
+    printf("Enter the element: ");
+    scanf("%d",&val);
+    CLL *ptr = (CLL *)malloc(sizeof(CLL));
+    ptr->data = val;
+
+    if (*start == NULL)
+    {
+        *start = ptr;
+        ptr->next = ptr;
+        return;
+    }
+
+    CLL *p = *start;
+    while(p->next!=*start)
+    {
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = *start;
+}
+void display(CLL *start)
+{
+    if (start == NULL)
+    {
+        return;
+    }
+    printf("The elements in the circular linked list are \n");
+    printf("%d ",start->data);
+    for (CLL *p = start->next; p!= start; p = p->next)
+    {
+        printf("%d ",p->data);
+    }
+    printf("\n");
+}
+void main()
+{
+    CLL *head = NULL;
+    char ch;
+    do
+    {
+        printf("Do you want to add an item(y/n): ");
+        scanf(" %c", &ch);
+        if (ch == 'y' || ch == 'Y')
+        {
+            insEnd(&head);
+            display(head);
+        }
+    } while (ch == 'y' || ch == 'Y');
+    display(head);
+
+}
