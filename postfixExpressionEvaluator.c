94242472c12e25a3f8880521b80c5a0444eb97f7
Jeeva8525 <jeevananthan8507@gmail.com>
2025-08-04 19:47:55 +0530

    evaluates the give postfix expression given  to the calculated value

---
+++ postfixExpressionEvaluator.c
@@ -0,0 +1,92 @@
+//Give the input with spaces between every single data item like 2 16 + 3 4 * -
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+typedef struct stack
+{
+    int data;
+    struct stack *next;
+} stack;
+stack *top = NULL;
+void push(int val)
+{
+    stack *newNode = (stack *)malloc(sizeof(stack));
+    newNode->data = val;
+    newNode->next = top;
+    top = newNode;
+}
+int pop()
+{
+    int elt;
+    if (top == NULL)
+    {
+        printf("The stack is already empty\n");
+        return 0;
+    }
+    elt = top->data;
+    stack *temp = top;
+    top = top->next;
+    free(temp);
+    return elt;
+}
+void displayStack()
+{
+    printf("The stack elements are\n");
+    for (stack *p = top; p != NULL; p = p->next)
+    {
+        printf("%5d \n", p->data);
+    }
+}
+int calculateExp(char exp[])
+{
+    int a, b;
+    char op;
+    for (int x = 0; x < strlen(exp); x++)
+    {
+        if (isdigit(exp[x]))
+        {
+            push(exp[x] - '0');
+        }
+        else
+        {
+            b = pop();
+            a = pop();
+            op = exp[x];
+            switch (op)
+            {
+            case '+':
+                push(a + b);
+                break;
+            case '-':
+                push(a - b);
+                break;
+            case '*':
+                push(a * b);
+                break;
+            case '^':
+                push(a ^ b);
+                break;
+            case '/':
+                if (b != 0)
+                {
+                    push(a / b);
+                    break;
+                }
+                else
+                {
+                    printf("Division Error: division by 0 occured\n");
+                    return 0;
+                }
+            }
+        }
+    }
+    return pop();
+}
+int main()
+{
+    char expression[100];
+    printf("Enter the expression: ");
+    scanf("%s", expression);
+    printf("The evaluated answer is %d", calculateExp(expression));
+}
